97d7c1dd45dc58559a28ba8eb61aa843fd4ae8ab
Thales H. Paloski <thaleshenriquepaloski1@gmail.com>
2026-02-09 13:50:15 -0300

    :sparkles: feat: add modelo eventos e testes do modelo

---
+++ src/models/evento.js
@@ -0,0 +1,37 @@
+/* eslint-disable camelcase */
+/* eslint no-unused-vars: */
+import db from '../db/dbconfig';
+
+class Evento {
+  constructor({
+    id,
+    nome,
+    descricao,
+    data,
+    autor_id,
+    created_at,
+    updated_at,
+  }) {
+    this.id = id || null;
+    this.nome = nome;
+    this.descricao = descricao;
+    this.data = data;
+    this.autor_id = autor_id;
+    this.created_at = created_at || new Date().toISOString();
+    this.updated_at = updated_at || new Date().toISOString();
+  }
+
+  static async pegarEventos() {
+    return [{
+      id: 1,
+      nome: 'Evento teste',
+      descricao: 'Descrição teste evento',
+      data: '2026-03-12',
+      autor_id: 1,
+      created_at: '2026-02-09 07:00:00',
+      updated_at: '2026-02-09 07:00:00',
+    }];
+  }
+}
+
+export default Evento;
