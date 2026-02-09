ecb5394c1a7a54f6846223f151da5eb446a82375
Thales H. Paloski <thaleshenriquepaloski1@gmail.com>
2026-02-09 15:39:58 -0300

    :sparkles: add: feature-flag para /eventos e testes de /eventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,12 +1,18 @@
 import Evento from '../models/evento.js';
 
 class EventosControllers {
+  static liberaAcessoEventos = () => process.env.EVENTO_FLAG === 'true';
+
   static pegarEventos = async (req, res) => {
-    try {
-      const resultado = await Evento.pegarEventos();
-      return res.status(200).json(resultado);
-    } catch (error) {
-      return res.status(500).json(error.message);
+    if (this.liberaAcessoEventos()) {
+      try {
+        const resultado = await Evento.pegarEventos();
+        return res.status(200).json(resultado);
+      } catch (error) {
+        return res.status(500).json(error.message);
+      }
+    } else {
+      return res.status(404).send();
     }
   };
 }
