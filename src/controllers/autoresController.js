36adaa09ab85b876d13046e603156dbe63c4cf82
Thales H. Paloski <thaleshenriquepaloski1@gmail.com>
2026-02-09 09:41:34 -0300

    feat: add méotodo livrosPorAutor

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -68,6 +68,18 @@ class AutoresController {
       return res.status(500).json(err.message);
     }
   };
+
+  static listarLivrosPorAutor = async (req, res) => {
+    const { params } = req;
+    try {
+      const listaDeLivros = await Autor.pegarLivrosPorAutor(params.id);
+      const autor = await Autor.pegarPeloId(params.id);
+      const resposta = { autor, livros: listaDeLivros };
+      return res.status(200).json(resposta);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  };
 }
 
 export default AutoresController;
